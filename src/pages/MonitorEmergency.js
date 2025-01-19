6f05f87a4784649cb4fd7e9484a3d43b5f975e1e
irtongco <irtongco@up.edu.ph>
2025-01-19 18:19:08 +0800

    Update MonitorEmergency.js

--- src/pages/MonitorEmergency.js
+++ src/pages/MonitorEmergency.js
@@ -37,7 +37,7 @@ const MonitorEmergency = () => {
     const interval = setInterval(fetchUnhandledAlerts, 5000);
 
     // Establish WebSocket connection
-    const socket = new WebSocket("ws://localhost:3000");
+    const socket = new WebSocket("ws://178.128.19.209:3000");
 
     socket.onopen = () => {
       console.log("WebSocket connected");
@@ -67,7 +67,7 @@ const MonitorEmergency = () => {
   // Fetch unhandled and handled alerts (excluding completed ones)
   const fetchUnhandledAlerts = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/api/unhandled-alerts");
+      const response = await axios.get("http://178.128.19.209:3000/api/unhandled-alerts");
       const filteredAlerts = response.data.filter(alert => alert.status !== "C"); // Keep handled/unhandled, remove completed
       setAlerts(filteredAlerts);
     } catch (error) {
@@ -82,7 +82,7 @@ const MonitorEmergency = () => {
 
     // Send request to update timestamp_handled (without marking as completed)
     try {
-      await axios.post("http://localhost:3000/api/update-handled-time", { alert_id: alert.alert_id });
+      await axios.post("http://178.128.19.209:3000/api/update-handled-time", { alert_id: alert.alert_id });
 
       // Update status in UI (to reflect handling time)
       setAlerts((prevAlerts) =>
@@ -98,7 +98,7 @@ const MonitorEmergency = () => {
   // Mark alert as completed
   const handleCompleteAlert = async (alert_id) => {
     try {
-      await axios.post("http://localhost:3000/api/complete-alert", { alert_id });
+      await axios.post("http://178.128.19.209:3000/api/complete-alert", { alert_id });
 
       // Remove completed alert from UI
       setAlerts((prevAlerts) => prevAlerts.filter((alert) => alert.alert_id !== alert_id));
